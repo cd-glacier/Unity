9a633ce89e2123ddb5c7f7a2cfe37744b19afab0
hyoga <hyoga0216@gmail.com>
2015-11-12 19:43:24 +0900

    first commit

---
+++ FirstTutorial/Roll-a-ball/Assets/Script/DangerWall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DangerWall : MonoBehaviour {
+
+	public GameObject gameOverLabelObject;
+
+	void OnCollisionEnter(Collision hit){
+		if(hit.gameObject.CompareTag("Player")){
+			gameOverLabelObject.SetActive(true);
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+#Unity projects
+
+## roll-a-ball
+
+ > https://github.com/unity3d-jp/FirstTutorial/wiki
+
+## 2D-shooting-game
+
+ > https://github.com/unity3d-jp-tutorials/2d-shooting-game/wiki 
