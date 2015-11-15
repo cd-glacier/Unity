f5a7e8bd768f5ff58625c9c39a77148241d0b730
hyoga <hyoga0216@gmail.com>
2015-11-15 13:57:12 +0900

    apply for android device

--- 2d_shooting/2d_ShootingGame/Assets/Scripts/Manager.cs
+++ 2d_shooting/2d_ShootingGame/Assets/Scripts/Manager.cs
@@ -6,15 +6,25 @@ public class Manager : MonoBehaviour {
 	public GameObject player;
 	private GameObject title;
 
+
 	// Use this for initialization
 	void Start () {
 		title = GameObject.Find("Title");
 	}
 	
+	void OnGUI(){
+		if(IsPlaying() == false && Event.current.type == EventType.MouseDown){
+			GameStart();
+		}
+	}
+	
 	// Update is called once per frame
 	void Update () {
-		if(IsPlaying() == false && Input.GetKeyDown(KeyCode.X)){
-			GameStart();
+		for(int i = 0; i < Input.touchCount; i++){
+			Touch touch = Input.GetTouch(i);
+			if(IsPlaying() == false && touch.phase == TouchPhase.Began){
+				GameStart();
+			}
 		}
 	}
 
